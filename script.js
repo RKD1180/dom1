bee296ec849c929e16a2fb5992b1c6da34f6bb95
Raton <ratonkumar3267@gmail.com>
2022-11-14 22:26:04 +0600

    14-11

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-console.log("It is from external 88");
+// console.log("It is from external 88");
 
-for (let i = 0; i < 10; i++) {
-  console.log(i);
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// const phone = { color: "red", price: 300 };
+// console.log(phone.price);
+function logName() {
+  console.log("Hello world");
+}
+const yellowId = document.getElementById("make-yellow");
+console.log(yellowId);
+//  click event only by function name
+yellowId.onclick = makeYellow;
+
+function makeYellow() {
+  document.body.style.backgroundColor = "yellow";
 }
 
-const phone = { color: "red", price: 300 };
-console.log(phone.price);
+// addeventlistener by click button
